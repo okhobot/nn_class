d749a86cdbefe25be4503a551e3b4d246155eb53
okhobot <okhobot@users.noreply.github.com>
2025-05-18 14:48:40 +0300

    log loss virtual marks deleted

--- nn_class/include/losses/LogLoss.hpp
+++ nn_class/include/losses/LogLoss.hpp
@@ -17,7 +17,7 @@ public:
         inline_add_loss_gradient="((output[i]-layer_res[i])/(layer_res[i]*(1-layer_res[i])))";
     }
 
-    virtual float calculate_error(float *output, float *layer_res, const nn_size_type &output_size)override
+    float calculate_error(float *output, float *layer_res, const nn_size_type &output_size)override
     {
         float res=0;
         for(nn_size_type i=0; i<output_size; i++)
@@ -25,7 +25,7 @@ public:
         return res;
     }
 
-    virtual void add_loss_gradient(const float &output, const float &layer_res, neuron *neuron)override
+    void add_loss_gradient(const float &output, const float &layer_res, neuron *neuron)override
     {
         neuron->gradient+=(output-layer_res)/(layer_res*(1-layer_res));
     }
